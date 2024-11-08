908ba16f160f6a6370abc1c5de9115d6943e28af
aldiver <agalcoriza@dswd.gov.ph>
2024-11-09 04:25:07 +0800

    s

--- tests/rfid.py
+++ tests/rfid.py
@@ -1,25 +1,14 @@
-import hid
-import time
+from rfidhid.core import RfidHid
 
-def read_rfid_device(device_path='/dev/hidraw0'):
-    # Open the HID device
-    try:
-        h = hid.device()
-        h.open_path(device_path)
-        print(f"Opened device: {device_path}")
+try:
+    # Try to open RFID device using default vid:pid (ffff:0035)
+    rfid = RfidHid()
+except Exception as e:
+    print(e)
+    exit()
 
-        print("Waiting for RFID scan...")
-        while True:
-            # Read from the device
-            data = h.read(64)  # Read up to 64 bytes (size may vary)
-            if data:
-                # Process data
-                print("Scanned RFID:", data)
-                # Here you would decode or process the RFID data
-                # For example, if it's in a particular format, decode it
+payload_response = rfid.read_tag()
+uid = payload_response.get_tag_uid()
 
-    except Exception as e:
-        print(f"Error reading from device: {e}")
-
-if __name__ == "__main__":
-    read_rfid_device('/dev/hidraw0')
+rfid.beep()
+print(uid)
