40437e8f61379c9cf2ddec7557c17b29def12f4b
aldiver <agalcoriza@dswd.gov.ph>
2024-11-09 01:02:44 +0800

    new servo

--- tests/servo.py
+++ tests/servo.py
@@ -1,48 +1,31 @@
 import time
-import board
-import busio
-from adafruit_pca9685 import PCA9685
+from adafruit_servokit import ServoKit
 
-# Set up I2C bus and PCA9685
-i2c = busio.I2C(board.SCL, board.SDA)
-pca = PCA9685(i2c)
-pca.frequency = 50  # Set frequency to 50Hz for servos
-
-# Helper function to set servo angle
-def set_servo_angle(channel, angle):
-    # Map angle (0 to 180) to PWM pulse width (servo-specific)
-    pulse_min = 1000  # Adjust these values based on servo specifications
-    pulse_max = 2000
-    pulse_width = pulse_min + (angle / 180.0) * (pulse_max - pulse_min)
-    pulse_length = int(pulse_width / 1000000 * pca.frequency * 4096)
-    pca.channels[channel].duty_cycle = pulse_length
-    print(f"Servo {channel} set to {angle} degrees (Pulse width: {pulse_length})")
+# Set up the ServoKit instance with 16 channels (this supports up to 16 servos)
+kit = ServoKit(channels=16)
 
 try:
     while True:
         # Move servos to 0 degrees
         print("Moving servos to 0 degrees...")
-        set_servo_angle(0, 0)
-        set_servo_angle(4, 0)
-        set_servo_angle(8, 0)
+        kit.servo[0].angle = 0
+        kit.servo[4].angle = 0
+        kit.servo[8].angle = 0
         time.sleep(1)
 
         # Move servos to 90 degrees
         print("Moving servos to 90 degrees...")
-        set_servo_angle(0, 90)
-        set_servo_angle(4, 90)
-        set_servo_angle(8, 90)
+        kit.servo[0].angle = 90
+        kit.servo[4].angle = 90
+        kit.servo[8].angle = 90
         time.sleep(1)
 
         # Move servos to 180 degrees
         print("Moving servos to 180 degrees...")
-        set_servo_angle(0, 180)
-        set_servo_angle(4, 180)
-        set_servo_angle(8, 180)
+        kit.servo[0].angle = 180
+        kit.servo[4].angle = 180
+        kit.servo[8].angle = 180
         time.sleep(1)
 
 except KeyboardInterrupt:
     print("Program stopped")
-finally:
-    pca.deinit()
-    print("PCA9685 deinitialized")
